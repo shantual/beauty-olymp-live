3ddba5ad4ea88cd42c8ba1681862707ec56c41f0
shantual <shantual@mail.ru>
2026-02-19 12:05:26 +0300

    Update upload.js

--- pages/api/uploads/upload.js
+++ pages/api/uploads/upload.js
@@ -178,6 +178,7 @@ export default async function handler(req, res) {
 
     return res.status(200).json({
       record: inserted,
+      objectUrl: objectUrl,  
       url: objectUrl,
       key: objectKey,
     });
