1ef8a28aa3436b16ecb84be169d0d0c970703432
shantual <shantual@mail.ru>
2026-02-19 16:19:48 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
             value:
-              "frame-ancestors 'self' https://basmanovatatyana.getcourse.ru https://beup.beolymp.ru;",
+              "frame-ancestors 'self' https://basmanovatatyana.getcourse.ru https://beup.beolymp.ru https://1olymp.ru https://www.1olymp.ru/;",
           },
         ],
       },
